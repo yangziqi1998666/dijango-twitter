6760053546ddbb82191a74837464a7b5e1307a0a
Ziqi Yang <yangziqi@usc.edu>
2022-01-07 21:21:20 -0800

    initial django

---
+++ .pycharm_helpers/python_stubs/1195854729/_codecs_cn.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+# module _codecs_cn
+# from /usr/lib/python3.6/lib-dynload/_codecs_cn.cpython-36m-x86_64-linux-gnu.so
+# by generator 1.147
+# no doc
+# no imports
+
+# functions
+
+def getcodec(*args, **kwargs): # real signature unknown
+    pass
+
+# no classes
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x7fcb734429e8>'
+
+__map_gb18030ext = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fcb723142a0>'
+
+__map_gb2312 = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fcb72314180>'
+
+__map_gbcommon = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fcb72314270>'
+
+__map_gbkext = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fcb72314150>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='_codecs_cn', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x7fcb734429e8>, origin='/usr/lib/python3.6/lib-dynload/_codecs_cn.cpython-36m-x86_64-linux-gnu.so')"
+
